9013fa6aa362b1a16bb09d52313be158812e027a
santhoshcad01@gmail.com <santhoshcad01@brightskills.com>
2024-01-05 18:04:33 +0530

    added to js files

---
+++ index.js
@@ -0,0 +1,34 @@
+a=60;
+
+d=55;
+d=Number(d);
+a=Number(a);
+a=a+d;
+console.log(a);
+
+var x=true;
+console.log(x, typeof x);
+x=Number(x);
+console.log(x, typeof x);
+
+
+// const a=10;
+
+const f={name:"dani", age:45}
+console.log(f, typeof f);
+f.name="danidanils";
+console.log(f, typeof f);
+
+let v1=10;
+let v2=20;
+ console.log(v1+v2);
+ console.log(v1-v2);
+ console.log(v1*v2);
+ console.log(v1^v2);
+ console.log(v1/v2);
+ console.log(v1%v2);
+ console.log(v1**v2);
+ console.log(v1&v2);
+
+
+
