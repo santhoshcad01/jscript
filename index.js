b4902e5e07627ca77e5a4cbb415af4ebdd10e338
santhoshcad01@gmail.com <santhoshcad01@brightskills.com>
2024-01-06 18:06:56 +0530

    file added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-a=60;
+/*a=60;
 
 d=55;
 d=Number(d);
@@ -31,4 +31,35 @@ let v2=20;
  console.log(v1&v2);
 
 
+ v1=++v1;
+ console.log(v1);
+ v1=--v1;
+ console.log(v1);
 
+ v1=25;
+ console.log(v1);
+  v1+=20; //v1=v1+20;
+ console.log(v1);
+ v1-=20; //v1=v1-v2;
+ console.log(v1);
+ 
+ v1/=20;
+ console.log(v1);
+
+ //Compression operator
+
+ console.log(v1==5);
+ console.log(v=="5",typeof v1);
+ console.log(v1!=1);
+ console.log(v1!=="5", typeof v1);
+ */
+
+ v1="s";
+ console.log(7==5 || 7<2 || 7>5);
+ console.log(v1=="s");
+ console.log(!(v1=="s"));
+
+ console.log(v1);
+ var output=v1=="s"? "v1 has value" : "v1 doesnot have value";
+ console.log(output);
+ 
